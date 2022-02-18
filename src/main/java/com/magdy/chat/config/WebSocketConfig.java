90650cc149370b986714e0c2402a4817696e7ed5
mady shenoda <markobosh5@gmail.com>
2022-02-18 10:20:49 +0300

    init

---
+++ src/main/java/com/magdy/chat/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.magdy.chat.config;
+
+import com.magdy.chat.interceptor.HttpHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors((HandshakeInterceptor) handshakeInterceptor);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
